f23fd912696d4458ac39915e444e25c8d392c166
Dennis Tran <trandennis0@gmail.com>
2019-07-11 23:38:18 -0700

    tetris jq

--- app.js
+++ app.js
@@ -1,284 +1,251 @@
- const tetrominos = []
- let currentTetromino;
- const height = 16
- const width = 10
- const gameState = 1 //play pause game over
- const colors = ["blue", "red", "orange", "yellow", "green", "black"]
- const move = 0
- const filledSpace = []
- let direction = ""
- const score = 0
+var tetris = {};
 
- const makePlayField = () => {
-     const playfield = document.querySelector('#tetris-board')
-     playfield.innerText = ''
-     let counter = 0
-     for(h = 0; h < height; h++) {
-       const row = document.createElement('tr')
-       row.className = "row"
-       row.dataset.row = h
-       for(w = 0; w < width; w++) {
-         const space = document.createElement('td')
-         space.className = "space"
-         space.dataset.w = w
-         space.dataset.h = h
-         space.dataset.index = counter
-         space.dataset.state = 0
-         space.innerText = "0: " + counter
-         row.appendChild(space)
-         counter ++
-       }
-       playfield.appendChild(row)
-     }
- };
+//Draw the grid
+tetris.drawPlayField = function(){
+	for(var row=0;row<22;row++){
+		$('#playfield').append('<tr class="'+row+'"></tr>');
+		for (var col=0;col<10;col++){
+			$('tr.'+row).append('<td id="'+col+'"></td>');
+		}
+	}
+}
 
- //makePlayField();
-
- const tetrominoShape = () => {
-     let t = [[1, 0], [0,1], [1,1],[2,1]]  //T
-     let i = [[0, 0], [0, 1], [0, 2], [0, 3]] // line
-     let o = [[0, 0], [0, 1], [1, 0], [1, 1]]  //square
-     let l = [[2,0], [0, 1], [1, 1], [2,1]]  //L
-     let s = [[0,0], [1,0], [1,1], [2,1]]  //S
-     let j = [[2,1], [1,1], [1,0], [2,0]]  //J
-     tetrominos.push(t);
-     tetrominos.push(i);
-     tetrominos.push(o);
-     tetrominos.push(l);
-     tetrominos.push(s);
-     tetrominos.push(j);
- };
-
- //tetrominoShape();
-
- const createTetromino = () => {
-   let randTetro = Math.floor(Math.random() * tetrominos.length)
-   let randColor = Math.floor(Math.random() * colors.length)
-   const center = Math.floor(width / 2)
-   let tetromino = tetrominos[randTetro]
-   const location = [center, 0]
-
-   currentTetromino = {
-     tetromino: tetromino,
-     color: colors[randColor],
-     location: location,
-     //index: blockCoordinates(tetromino, location)  //used for collision detection
-   }
- };
-
- const drawTetromino = () => {
-   if (!hitGridWall()){
-   //drawing tetris block onto playfield
-   let tetromino = currentTetromino.tetromino
-   let location = currentTetromino.location
-
-   clearCurrent() //clear current block and update status
-
-   if(direction === 'down') {
-     currentTetromino.location[1]++
-   }
-   else if(direction === 'left' && !hitGridWall()) {
-     currentTetromino.location[0]--
-   }
-   else if(direction === 'right' && !hitGridWall()) {
-     currentTetromino.location[0]++
-   }
-
-   //redraw new block
-
-   for(i = 0; i < tetromino.length; i++) {
-       let w = tetromino[i][0] + location[0]
-       let h = tetromino[i][1] + location[1]
-       let space = document.querySelector('[data-w="'+ w +'"][data-h="'+ h +'"]')
-       space.classList.add('filled')
-       space.style.backgroundColor = currentTetromino.color
-   }
- }
- };
-
- function clearCurrent()
- {
-      //reset all blocks
-     let tetromino = currentTetromino.tetromino;
-     let location = currentTetromino.location;
-
-     for(let i = 0; i < tetromino.length; i++)
-     {
-         let w = tetromino[i][0] + location[0];
-         let h = tetromino[i][1] + location[1];
-         let space = document.querySelector('[data-w="' + w + '"][data-h="' + h + '"]');
-         space.classList.remove('filled');
-         space.style.backgroundColor="";
-     }
-     //currentTetromino.index = blockCoordinates(currentTetromino.tetromino, currentTetromino.location)
- }
+//Variable to store current coordiates
+tetris.origin = {row:2,col:5};
+tetris.currentShape = 'I';
+tetris.currentCoor;
 
 
- function checkKey(e) {
-     e.preventDefault();
+//Fill the cells
+tetris.fillCells = function(coordinates,fillColor){
+	for(var i=0;i<coordinates.length;i++){
+		var row = coordinates[i].row;
+		var col = coordinates[i].col;
+		var $coor = $('.'+row).find('#'+col);
+		$coor.attr('bgcolor',fillColor);
+	}
+}
 
-     e = e || window.event;
+//Move current shape
+tetris.move = function(direction){
+	this.fillCells(this.currentCoor,'');
 
-     if (e.keyCode == '40') {
-          //down arrow
-         direction="down";
-     }
-     else if (e.keyCode == '37') {
-          //left arrow
-         direction="left";
-     }
-     else if (e.keyCode == '39') {
-          //right arrow
-         direction="right";
-     }
+	//move origin
+	if(direction === 'right'){
+		this.origin.col++;
+	} else if (direction === 'left'){
+		this.origin.col--;
+	}
 
-     drawTetromino();
- }
+	this.currentCoor = this.shapeToCoor(this.currentShape,this.origin);
 
- function clearCoordinates() {
-   const spaces = document.querySelectorAll(".space")
-   for(let i = 0; i < spaces.length; i++) {
-     spaces[i].innerText = ""
-   }
- }
+	if(this.ifReverse()){
+		if(direction === 'right'){
+			this.origin.col--;
+		} else if (direction === 'left'){
+			this.origin.col++;
+		}
+	}
 
- function hitGridWall() {
-     // check if the current block is at edge already
-     // if any block in shape has an h index of 0 or width - 1
-     // or if any element to the left of right is occupied
-     let blocks = currentTetromino.tetromino;
-     let offset = currentTetromino.location;
-     let collision = '';
+	this.currentCoor = this.shapeToCoor(this.currentShape,this.origin);
 
-     // let pos = document.querySelectorAll('.filled')
-     //
-     // pos.forEach(function(coord){
-     //   let cord = pos.getAttribute('data-w');
-     //
-     //   console.log(coord);
-     //   console.log(cord);
-     // })
-       // let big = document.querySelector('[data-h="' + h + '"]')
-       //let fill = pos.ClassName
-       // let stack = true
-       // if(Math.max(big) + 1 === fill){
-       //   console.log(coord);
-       //   return stack
-       // }
-     //   console.log(coord);
-     //   console.log(big);
-     // })
-     // console.log(pos)
-     //console.log(yes);
+	this.fillCells(this.currentCoor,'black');
+}
 
-     for(let i = 0; i < blocks.length; i++){
-        let block = blocks[i];
-        let w = block[0] + offset[0];
-        let h = block[1] + offset[1];
+//Rotate current shape
+tetris.rotate = function(){
+	var lastShape = this.currentShape;
+	this.fillCells(this.currentCoor,'');
+
+	if(this.currentShape === 'L'){
+		this.currentShape = 'L90';
+	} else if(this.currentShape === 'L90'){
+		this.currentShape = 'L180';
+	} else if(this.currentShape === 'L180'){
+		this.currentShape = 'L270';
+	} else if(this.currentShape === 'L270'){
+		this.currentShape = 'L';
+	} else if(this.currentShape === 'J'){
+		this.currentShape = 'J90';
+	} else if(this.currentShape === 'J90'){
+		this.currentShape = 'J180';
+	} else if(this.currentShape === 'J180'){
+		this.currentShape = 'J270';
+	} else if(this.currentShape === 'J270'){
+		this.currentShape = 'J';
+	} else if(this.currentShape === 'I'){
+		this.currentShape = 'I90';
+	} else if(this.currentShape === 'I90'){
+		this.currentShape = 'I';
+	} else if(this.currentShape === 'S'){
+		this.currentShape = 'S90';
+	} else if(this.currentShape === 'S90'){
+		this.currentShape = 'S';
+	} else if(this.currentShape === 'Z'){
+		this.currentShape = 'Z90';
+	} else if(this.currentShape === 'Z90'){
+		this.currentShape = 'Z';
+	} else if(this.currentShape === 'T'){
+		this.currentShape = 'T90';
+	} else if(this.currentShape === 'T90'){
+		this.currentShape = 'T180';
+	} else if(this.currentShape === 'T180'){
+		this.currentShape = 'T270';
+	} else if(this.currentShape === 'T270'){
+		this.currentShape = 'T';
+	}
+
+	this.currentCoor = this.shapeToCoor(this.currentShape,this.origin);
+
+	for(var i=0;i<this.currentCoor.length;i++){
+		if(this.ifReverse()){
+			this.currentShape = lastShape;
+		}
+	}
+
+	this.currentCoor = this.shapeToCoor(this.currentShape,this.origin);
+	this.fillCells(this.currentCoor,'black');
+}
+
+//Define all shapes
+tetris.shapeToCoor = function(shape,origin){
+	if(shape === 'L'){
+		return [{row:origin.row,col:origin.col},{row:origin.row-1,col:origin.col},{row:origin.row+1,col:origin.col},{row:origin.row+1,col:origin.col+1}]
+	} else if(shape === 'J'){
+		return [{row:origin.row,col:origin.col},{row:origin.row-1,col:origin.col},{row:origin.row+1,col:origin.col},{row:origin.row+1,col:origin.col-1}]
+	} else if(shape === 'I'){
+		return [{row:origin.row,col:origin.col},{row:origin.row-1,col:origin.col},{row:origin.row-2,col:origin.col},{row:origin.row+1,col:origin.col}]
+	} else if(shape === 'O'){
+		return [{row:origin.row,col:origin.col},{row:origin.row-1,col:origin.col},{row:origin.row-1,col:origin.col+1},{row:origin.row,col:origin.col+1}]
+	} else if(shape === 'S'){
+		return [{row:origin.row,col:origin.col},{row:origin.row-1,col:origin.col},{row:origin.row,col:origin.col-1},{row:origin.row-1,col:origin.col+1}]
+	} else if(shape === 'T'){
+		return [{row:origin.row,col:origin.col},{row:origin.row-1,col:origin.col},{row:origin.row,col:origin.col-1},{row:origin.row,col:origin.col+1}]
+	} else if(shape === 'Z'){
+		return [{row:origin.row,col:origin.col},{row:origin.row-1,col:origin.col},{row:origin.row-1,col:origin.col-1},{row:origin.row,col:origin.col+1}]
+	} else if(shape === 'L90'){
+  	return [{row:origin.row,col:origin.col},{row:origin.row,col:origin.col+1},{row:origin.row,col:origin.col-1},{row:origin.row+1,col:origin.col-1}];
+  } else if(shape === 'L180'){
+  	return [{row:origin.row,col:origin.col},{row:origin.row-1,col:origin.col},{row:origin.row+1,col:origin.col},{row:origin.row-1,col:origin.col-1}];
+  } else if(shape === 'L270'){
+  	return [{row:origin.row,col:origin.col},{row:origin.row,col:origin.col+1},{row:origin.row,col:origin.col-1},{row:origin.row-1,col:origin.col+1}];
+  } else if(shape === 'J90'){
+		return [{row:origin.row,col:origin.col},{row:origin.row,col:origin.col-1},{row:origin.row,col:origin.col+1},{row:origin.row-1,col:origin.col-1}]
+	} else if(shape === 'J180'){
+		return [{row:origin.row,col:origin.col},{row:origin.row-1,col:origin.col},{row:origin.row+1,col:origin.col},{row:origin.row-1,col:origin.col+1}]
+	} else if(shape === 'J270'){
+		return [{row:origin.row,col:origin.col},{row:origin.row,col:origin.col-1},{row:origin.row,col:origin.col+1},{row:origin.row+1,col:origin.col+1}]
+	} else if(shape === 'I90'){
+		return [{row:origin.row,col:origin.col},{row:origin.row,col:origin.col-1},{row:origin.row,col:origin.col+1},{row:origin.row,col:origin.col+2}]
+	} else if(shape === 'S90'){
+		return [{row:origin.row,col:origin.col},{row:origin.row-1,col:origin.col},{row:origin.row-1,col:origin.col-1},{row:origin.row-2,col:origin.col-1}]
+	} else if(shape === 'Z90'){
+		return [{row:origin.row,col:origin.col},{row:origin.row-1,col:origin.col},{row:origin.row-1,col:origin.col+1},{row:origin.row-2,col:origin.col+1}]
+	} else if(shape === 'T90'){
+		return [{row:origin.row,col:origin.col},{row:origin.row-1,col:origin.col},{row:origin.row+1,col:origin.col},{row:origin.row,col:origin.col+1}]
+	} else if(shape === 'T180'){
+		return [{row:origin.row,col:origin.col},{row:origin.row+1,col:origin.col},{row:origin.row,col:origin.col-1},{row:origin.row,col:origin.col+1}]
+	} else if(shape === 'T270'){
+		return [{row:origin.row,col:origin.col},{row:origin.row-1,col:origin.col},{row:origin.row+1,col:origin.col},{row:origin.row,col:origin.col-1}]
+	}
+}
+
+//Drop shape by one row
+tetris.drop = function(){
+	var reverse = false;
+
+	this.fillCells(this.currentCoor,'');
+	this.origin.row++;
+	for(var i=0;i<this.currentCoor.length;i++){
+		this.currentCoor[i].row++;
+		if(this.ifReverse()){
+			reverse = true;
+		}
+	}
+
+	if(reverse){
+		for(var i=0;i<this.currentCoor.length;i++){
+			this.currentCoor[i].row--;
+		}
+		this.origin.row--;
+	}
+
+	this.fillCells(this.currentCoor,'black');
+
+	if(reverse){
+		this.fillCells(this.currentCoor,'BLACK');
+		this.emptyFullRow();
+		this.spawn();
+	}
+
+}
+
+//Spawn random shape
+tetris.spawn = function(){
+	var random = Math.floor(Math.random()*7);
+	var shapeArray = ['L','J','I','O','S','T','Z'];
+	this.currentShape = shapeArray[random];
+	this.origin = {row:2,col:5};
+	this.currentCoor = this.shapeToCoor(this.currentShape,this.origin);
+}
+
+//If we need to reverse
+tetris.ifReverse = function(){
+	for(var i=0;i<this.currentCoor.length;i++){
+		var row = this.currentCoor[i].row;
+		var col = this.currentCoor[i].col;
+		var $coor = $('.'+row).find('#'+col);
+		if($coor.length === 0 || $coor.attr('bgcolor') === 'BLACK'){
+			return true;
+		}
+	}
+	return false;
+}
+
+//Empty full row
+tetris.emptyFullRow = function(){
+	var drops = 0;
+	for (var i=21; i>=0;i--){
+		var rowIsFull = true;
+
+		for (var j=0;j<10;j++){
+			var $coor = $('.'+i).find('#'+j);
+			if($coor.attr('bgcolor')!=='BLACK'){
+				rowIsFull = false;
+			}
+
+			if(drops>0){
+				var $newCoor = $('.'+(i+drops)).find('#'+j);
+				$newCoor.attr('bgcolor',$coor.attr('bgcolor'));
+			}
+		}
+
+		if(rowIsFull){
+			drops++;
+		}
+	}
+}
 
 
-        if (direction === "left")
-          w--;
-        else if (direction === "right")
-          w++
+$(document).ready(function(){
+	tetris.drawPlayField();
+	tetris.currentCoor = tetris.shapeToCoor(tetris.currentShape,tetris.origin);
+	tetris.fillCells(tetris.currentCoor,'black');
 
+	$(document).keydown(function(e){
+		if(e.keyCode === 39){
+			tetris.move('right');
+		} else if (e.keyCode === 37){
+			tetris.move('left');
+		} else if (e.keyCode === 38){
+			tetris.rotate();
+		} else if (e.keyCode === 40){
+			tetris.drop();
+		}
+	})
+8
+	var gravity = setInterval(function(){
+		tetris.drop();
+	},500);
 
-
-        if (direction === "down")
-          h++;
-
-        if (w < 0 && direction === "left") {
-          collision = "side";
-          break;
-        }
-
-        else if (w === width && direction === "right") {
-          collision = "side";
-          break;
-        }
-
-        let space = document.querySelector('[data-w="' + w + '"][data-h="' + h + '"]')
-        filledSpace.push(space)
-        if (filledSpace.indexOf(space.dataset.index) > -1) {
-          collision = "side"
-          break
-        }
-
-        // if(space.className === "space filled") {
-        //   collision = "bottom"
-        //   break
-        // }
-        //console.log(currentTetromino)
-        //console.log(space);
-        //filledSpace.forEach(space => console.log(space.dataset.w) )
-        //  ;
-        // console.log(filledSpace.indexOf(space.));
-
-
-
-        if(direction === "down") {
-          h++
-        }
-
-        //let index = document.querySelector('[data-w="' + w + '"][data-h="' + h + '"]')
-        //filledSpace.push(index)
-
-        if(h === height /*|| filledSpace.indexOf(index.dataset.index) > -1*/) {
-          collision = "bottom"
-          if(collision === "bottom"){
-            createTetromino()
-          }
-        }
-
-
-     }
-
-     return collision;
- }
-
- // function hitVertical() {
- //   let blocks = currentTetromino.tetromino;
- //   let offset = currentTetromino.location;
- //   let collision = '';
- //
- //   for(let i = 0; i < blocks.length; i++){
- //      let block = blocks[i];
- //      let w = block[0] + offset[0];
- //      let h = block[1] + offset[1];
- //
- //      if(direction === "down") {
- //        h++
- //      }
- //
- //      let index = document.querySelector('[data-w="' + w + '"][data-h="' + h + '"]')
- //      filledSpace.push(index)
- //
- //      if(h === height || filledSpace.indexOf(index.dataset.index) > -1) {
- //        collision = "bottom"
- //        break
- //      }
- //
- //      if(collision === "bottom"){
- //        createTetromino()
- //      }
- //
- //    }
- // }
-
-
- function start()
- {
-     makePlayField();
-     tetrominoShape();
-     createTetromino();
-     drawTetromino();
-     document.onkeydown = checkKey;
-     hitGridWall()
-//     hitVertical()
-     clearCoordinates()
-
- }
-
- window.addEventListener('load', function(){
-     start();
- });
+})
